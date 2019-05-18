406fedca01cae975aa91d9f4c65991f2dccfa547
Emerson <Houndlover32@gmail.com>
2019-05-18 17:36:33 -0400

    Added folder class

--- noteplus/commands/basis.py
+++ noteplus/commands/basis.py
@@ -3,8 +3,15 @@
 
 
 class Note:
-
+    """Note class"""
     def __init__(self, title, note, time_stamp):
         self.title = title
         self.note = note
         self.time_stamp = time_stamp
+
+
+class Folder:
+    """Folder class"""
+    def __init__(self, name, date_created):
+        self.name = name
+        self.date_created = date_created
