e9bc07cf1f90d247c80c4965b1e1d6f0c25adc45
Emerson <Houndlover32@gmail.com>
2019-05-17 20:18:43 -0400

    Restructured __main__.py

--- noteplus/__main__.py
+++ noteplus/__main__.py
@@ -2,14 +2,28 @@
 # Entry point of the noteplus cli application
 
 import click
-from .commands.version import version
-from.commands.hello import greeting
+
+from noteplus.commands.add import add
+from noteplus.commands.remove import remove
+from noteplus.commands.retrieve import retrieve
+import noteplus.commands.test as test
+
+from noteplus.setup_db import init_db
 
 
-@click.group()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.version_option(version='1.0.0', prog_name='noteplus')
+@click.group(context_settings=CONTEXT_SETTINGS)
 def main():
     """Simple note-taking utility"""
 
+    # Create the notes database if non-existent
+    init_db()
 
-main.add_command(version)
-main.add_command(greeting)
+
+main.add_command(add)
+main.add_command(remove)
+main.add_command(retrieve)
+main.add_command(test)
