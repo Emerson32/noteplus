3a927225e41fb8d748a57e039a267e18b81ff324
Emerson <Houndlover32@gmail.com>
2019-05-18 14:46:50 -0400

    Updated install requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     install_requires=[
         'Click',
         'prompt_toolkit',
-        'PyInquirer'
+        'PyInquirer',
+        'pyfiglet'
     ],
     entry_points={
         'console_scripts': [
