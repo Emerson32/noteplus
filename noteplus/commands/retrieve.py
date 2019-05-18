7e76c6e7ab6bbc65c2a3c988deedab76ab63d702
Emerson <Houndlover32@gmail.com>
2019-05-18 14:54:10 -0400

    Updated basic functionality

--- noteplus/commands/retrieve.py
+++ noteplus/commands/retrieve.py
@@ -9,9 +9,11 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command('retrieve', context_settings=CONTEXT_SETTINGS, short_help='retrieve note entries')
 @click.option('-a', '--all', 'all_notes', is_flag=True, help='retrieve all note entries')
+@click.option('-l', '--less', 'less', is_flag=True, help='show note(s) by using a pager')
 @click.option('-t', '--title', 'note_title', nargs=1, type=str, help='retrieve a note based on its title')
 @click.option('-n', '--note', 'note', nargs=1, type=str, help='retrieve a note based on the note')
-def retrieve(all_notes, note_title, note):
+def retrieve(all_notes, less, note_title, note):
+    """Retrieve a note from the notebook"""
     conn = sqlite3.connect('notes.db')
     c = conn.cursor()
 
@@ -20,11 +22,23 @@ def retrieve(all_notes, note_title, note):
             c.execute('SELECT * FROM notes')
 
         results = c.fetchall()
-        for item in results:
-            print()
-            print('Title: ' + item[0])
-            print('Note: ' + item[1])
-            print('Time: ' + item[2])
+
+        # Less pager selected
+        if less:
+            lines = ''
+
+            # Build the lines to be displayed
+            for item in results:
+                lines += 'Title: ' + item[0] + '\nNote: ' + item[1] + '\nTime: ' + item[2] + '\n\n'
+
+            click.echo_via_pager(lines)
+
+        else:
+            for item in results:
+                click.echo()
+                click.echo('Title: ' + item[0])
+                click.echo('Note: ' + item[1])
+                click.echo('Time: ' + item[2])
 
     elif note_title:
         with conn:
@@ -43,10 +57,10 @@ def retrieve(all_notes, note_title, note):
 
         results = c.fetchall()
         for item in results:
-            print()
-            print('Title: ' + item[0])
-            print('Note: ' + item[1])
-            print('Time: ' + item[2])
+            click.echo()
+            click.echo('Title: ' + item[0])
+            click.echo('Note: ' + item[1])
+            click.echo('Time: ' + item[2])
 
     else:
         raise click.UsageError('Missing option')
