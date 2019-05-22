24640e7eac320713bde9ec1461874f9bd84cdd9f
Emerson <Houndlover32@gmail.com>
2019-05-22 18:48:19 -0400

    added to_string()

--- noteplus/commands/basis.py
+++ noteplus/commands/basis.py
@@ -1,50 +1,23 @@
 # basis.py
 # Object representing each note entered by the user
-import click
-import sqlite3
 
 
 class Note:
     """Note class"""
-    def __init__(self, title, note, time_stamp):
+    def __init__(self, title, note, path, time_stamp):
         self.title = title
         self.note = note
+        self.path = path
         self.time_stamp = time_stamp
 
-    def add(editor, title, text):
-        """
-        Add a note to the database
-
-        :param editor: Visual editor flag
-        :param title: Title of the note to be added
-        :param text: Text of the note to be added
-        """
-
-    def remove(purge, title):
-        """
-        Remove a note
-
-        :param purge: Remove all entries in the database
-        :param title: Title of the note to be removed
-        """
-        conn = sqlite3.connect('notes.db')
-        c = conn.cursor()
-
-        if purge:
-            with conn:
-                c.execute('DELETE from notes')
-
-        elif title:
-            with conn:
-                c.execute('DELETE from notes WHERE title=:title',
-                        {'title': title})
-
-        else:
-            raise click.UsageError('Missing option or argument')
+    def to_string(self):
+        return 'Location: ' + self.path + '\nTitle: ' + self.title + '\nNote: ' + self.note +\
+               '\nTime Stamp: ' + self.time_stamp + '\n'
 
 
 class Folder:
     """Folder class"""
-    def __init__(self, name, date_created):
+    def __init__(self, name, path, date_created):
         self.name = name
+        self.path = path
         self.date_created = date_created
