f9223902bc25f481503bf25bc9c0da00ec4bc3b9
Emory Lindsey <emoryfpd@gmail.com>
2019-05-22 12:36:42 -0400

    Made remove function more object oriented

--- noteplus/commands/basis.py
+++ noteplus/commands/basis.py
@@ -1,5 +1,7 @@
 # basis.py
 # Object representing each note entered by the user
+import click
+import sqlite3
 
 
 class Note:
@@ -8,6 +10,37 @@ class Note:
         self.title = title
         self.note = note
         self.time_stamp = time_stamp
+
+    def add(editor, title, text):
+        """
+        Add a note to the database
+
+        :param editor: Visual editor flag
+        :param title: Title of the note to be added
+        :param text: Text of the note to be added
+        """
+
+    def remove(purge, title):
+        """
+        Remove a note
+
+        :param purge: Remove all entries in the database
+        :param title: Title of the note to be removed
+        """
+        conn = sqlite3.connect('notes.db')
+        c = conn.cursor()
+
+        if purge:
+            with conn:
+                c.execute('DELETE from notes')
+
+        elif title:
+            with conn:
+                c.execute('DELETE from notes WHERE title=:title',
+                        {'title': title})
+
+        else:
+            raise click.UsageError('Missing option or argument')
 
 
 class Folder:
--- noteplus/commands/remove.py
+++ noteplus/commands/remove.py
@@ -3,22 +3,14 @@
 import click
 import sqlite3
 
+from noteplus.commands.basis import Note
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command('remove', context_settings=CONTEXT_SETTINGS, short_help='remove a note')
 @click.option('-p', '--purge', 'purge', is_flag=True, help='remove all notes')
-@click.argument('title', required=False, type=str)
+@click.argument('title', required=False, default=None, type=str)
 def remove(purge, title):
-    conn = sqlite3.connect('notes.db')
-    c = conn.cursor()
-
-    if purge:
-        with conn:
-            c.execute('DELETE from notes')
-
-    elif title:
-        with conn:
-            c.execute('DELETE from notes WHERE title=:title',
-                      {'title': title})
+    """Remove a note from the database"""
+    Note.remove(purge=purge, title=title)
