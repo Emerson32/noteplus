4afae320910d268aff4633041483eb9e75c7711b
Emory Lindsey <emoryfpd@gmail.com>
2019-05-27 12:12:27 -0400

    Revised __main__.py

--- noteplus/__main__.py
+++ noteplus/__main__.py
@@ -8,6 +8,7 @@ import sys
 from pyfiglet import Figlet
 
 from noteplus.commands.add import add
+from noteplus.commands.edit import edit
 from noteplus.commands.remove import remove
 from noteplus.commands.retrieve import retrieve
 
@@ -20,8 +21,10 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.version_option(version='1.0.0', prog_name='noteplus')
 @click.group(invoke_without_command=True, context_settings=CONTEXT_SETTINGS)
-@click.option('-b', '--banner', 'banner', is_flag=True, help='display the banner')
-@click.option('-i', '--interactive', 'interactive', is_flag=True, help='interactive mode')
+@click.option('-b', '--banner', 'banner', is_flag=True,
+              help='display the banner')
+@click.option('-i', '--interactive', 'interactive',
+              is_flag=True, help='interactive mode')
 def main(banner, interactive):
     """Simple note-taking utility"""
 
@@ -37,5 +40,6 @@ def main(banner, interactive):
 
 
 main.add_command(add)
+main.add_command(edit)
 main.add_command(remove)
 main.add_command(retrieve)
