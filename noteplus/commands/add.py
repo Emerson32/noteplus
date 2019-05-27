11a60c0bccdd48478eca697c819177fbfb5b4791
Emory Lindsey <emoryfpd@gmail.com>
2019-05-27 12:14:18 -0400

    Added styling to note creation output

--- noteplus/commands/add.py
+++ noteplus/commands/add.py
@@ -1,37 +1,57 @@
 # add.py - Create a new entry through the usage of your default editor
 #           or through the command line
 import click
-from os import getcwd
+import os
 
 from noteplus.commands.operations import add_note, add_folder
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-@click.command('add', context_settings=CONTEXT_SETTINGS, short_help='add note entries')
-@click.option('-e', '--editor', 'editor', is_flag=True, help='use buffer to enter note')
-@click.option('-f', '--folder', 'folder', nargs=1, type=click.Path(writable=True),
-              help='Create a folder with a given name')
-@click.option('-n', '--note', 'note', is_flag=True, help='add a new note entry')
+@click.command('add', context_settings=CONTEXT_SETTINGS,
+               short_help='Add notes and/or folders')
+@click.option('-e', '--editor', 'editor', is_flag=True,
+              help='Use buffer to enter note')
+@click.option('-f', '--folder', 'folder', nargs=1,
+              type=str, help='Create a folder with a given name')
+@click.option('-n', '--note', 'note', is_flag=True,
+              help='Add a new note entry')
+@click.option('-p', '--path', 'path',
+              default=lambda: os.environ.get('PWD', ''),
+              show_default='current directory',
+              type=click.Path(writable=True),
+              help='Specified path')
 @click.argument('title', required=False, default='', type=str)
 @click.argument('text', required=False, default='', type=str)
-def add(editor, folder, note, title, text):
+def add(editor, folder, note, path, title, text):
     """Add a new note to the notebook"""
-    cwd = getcwd()
 
-    # Create the new folder and then insert the created note into the folder
     if folder and note:
-        folder_path = add_folder(folder)
+        # Create the folder with the given path.
+        # The path is the current directory by default.
+        add_folder(folder, path=path)
 
-        added_note = add_note(editor=editor, title=title, text=text, path=folder_path)
-        click.echo(added_note.to_string())
+        # In this case an extended path must be created
+        # so the file is stored within the created directory
+        file_path = os.path.join(path, folder)
+
+        # Unreachable path created
+        if not os.path.exists(file_path):
+            raise click.UsageError('No such file or directory')
+
+        added_note = add_note(editor=editor, title=title, text=text,
+                              path=file_path)
+
+        click.secho(added_note.to_string(), fg='green')
 
     elif folder:
-        add_folder(folder)
+        add_folder(folder, path=path)
 
     elif note:
-        added_note = add_note(editor=editor, title=title, text=text, path=cwd)
-        click.echo(added_note.to_string())
+        added_note = add_note(editor=editor, title=title, text=text,
+                              path=path)
+
+        click.secho(added_note.to_string(), fg='green')
 
     else:
         raise click.UsageError('Missing option')
