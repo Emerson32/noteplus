f42f1db2304605e64d0acd18ffdc46511ccf4500
Emerson <Houndlover32@gmail.com>
2019-06-13 23:11:14 -0400

    (remove.py) - Began optimizing removal logic

--- noteplus/commands/remove.py
+++ noteplus/commands/remove.py
@@ -19,11 +19,24 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('-s', '--subject', 'subject',
               nargs=1, type=click.Path(writable=True),
               help='remove a folder')
-@click.argument('nb_title', required=True, default='notes.db', type=str)
-def remove(clean, purge, subject, nb_title):
+@click.option('-n', '--note', 'note', nargs=1,
+              help='remove a note')
+@click.option('-nb', '--notebook', 'notebook',
+              nargs=1, type=str, default='notes.db',
+              help='Specify the name of the notebook file')
+@click.option('-p', '--path', 'path', nargs=1,
+              type=click.Path(writable=True),
+              default=lambda: os.environ.get('PWD', ''),
+              show_default='current directory',
+              help='specific folder path of note')
+def remove(clean, purge, subject, note, notebook, path):
     """Remove a note from the database"""
 
-    removed_notes = []
+    if os.path.exists(path):
+        os.chdir(path)
+        note_book = NoteBook(path=path, file_name=notebook)
+    else:
+        raise click.UsageError("No such file or directory")
 
     if subject:
         subject_name = os.path.basename(subject.rstrip('/'))
@@ -32,21 +45,20 @@ def remove(clean, purge, subject, nb_title):
         removed_subject = subj.remove()
         click.echo("Removed: " + removed_subject)
 
+    elif note:
+        note_book.remove_note(note)
+
+    elif clean:
+        if click.confirm('\nRemove all notes in the provided notebook?'):
+            note_book.clean_notes()
+
+    # TODO: Make the purge option remove all notebook files exclusively
+    elif purge:
+        if click.confirm('\nRemove all notebooks in the current subject?'):
+            note_book.purge_notes()
+
+    elif notebook:
+        note_book.remove()
+
     else:
-        note_book = NoteBook(os.getcwd(), nb_title)
-
-        if clean:
-            if click.confirm('\nRemove all notes in the current directory?'):
-                removed_notes = note_book.clean_notes()
-
-        if purge:
-            if click.confirm('\nRemove notes file?'):
-                removed_notes = note_book.purge_notes()
-
-# Print out removed notes
-    click.echo()
-    for item in removed_notes:
-        click.secho("Removed: ", bold=True, nl=False)
-        click.secho(item[0], fg='red', underline=True)
-    
-    click.echo()
+        raise click.BadOptionUsage('Missing option')
